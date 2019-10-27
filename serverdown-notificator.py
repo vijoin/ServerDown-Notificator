852024e9e90e730f66b98c5505be6693c8e5356d
Victor Inojosa <vijoin@gmail.com>
2019-10-27 13:07:36 -0300

    Get the sites and emails from files
    ie: `python serverdown-notificator.py -sf sites.txt -ef emails.txt`

--- serverdown-notificator.py
+++ serverdown-notificator.py
@@ -3,27 +3,53 @@ from requests.exceptions import ConnectionError
 import argparse
 
 parser = argparse.ArgumentParser(description='Notify if a site is Down!')
-parser.add_argument('-s', '--sites', nargs='+', help="i.e: -s http://127.0.0.1 http://domain.xyz", required=True)
-parser.add_argument('-e', '--emails', nargs='+', help="i.e: -e myemail@domain.xyz myboss@domain.xyz", required=True)
+
+### Receive sites and emails via CLI
+parser.add_argument('-s', '--sites', nargs='+', help="i.e: -s http://127.0.0.1 http://domain.xyz")
+parser.add_argument('-e', '--emails', nargs='+', help="i.e: -e myemail@domain.xyz myboss@domain.xyz")
+
+### Receive sites and email from a file
+parser.add_argument('-sf', '--sites-from-file', type=str, help='Pass the sites from a file')
+parser.add_argument('-ef', '--emails-from-file', type=str, help='Pass the emails from a file')
+
 args = parser.parse_args()
 
 
 def send_notification(emails, site):
     for email in emails:
         print(f"""
-        Hello {email}
+        Hello {email.rstrip()}
         The Site {site} is down!""")
 
 
-def main(sites, emails):
-    for site in sites:
+def _get_sites(f):
+    return f.readlines()
+
+
+def _get_emails(f):
+    return f.readlines()
+
+
+def main(**kwargs):
+    if 'sites_from_file' in kwargs:
+        print("OK")
+        file_path = kwargs.get('sites_from_file')
+        with open(file_path, 'r') as f:
+            kwargs['sites'] = _get_sites(f)
+
+    if 'emails_from_file' in kwargs:
+        file_path = kwargs.get('emails_from_file')
+        with open(file_path, 'r') as f:
+            kwargs['emails'] = _get_emails(f)
+
+    for site in kwargs['sites']:
         try:
-            r = requests.get(f'{site}', timeout=5)
+            r = requests.get(f'{site.rstrip()}', timeout=5)
             if r.status_code != 200:
-                send_notification(emails, site)
+                send_notification(kwargs['emails'], site)
         except ConnectionError:
             print("\nConnection Error\n")
-            send_notification(emails, site)
+            send_notification(kwargs['emails'], site)
 
 
 if __name__ == '__main__':
